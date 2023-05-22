03666000430ba5a152ce0227f5f49826148b334d
Scarlet <scarletborat@ukr.net>
2023-05-21 21:57:42 -0700

    Formalize approach, remove bs4, request dependencies. All logic based on selenium

--- video_converters.py
+++ video_converters.py
@@ -2,20 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
-import requests
-from bs4 import BeautifulSoup
 
-def snaptik_converter(link_to_video):
+def snaptik_converter(link_to_video, driver):
     url = "https://snaptik.app/"
     input_selector = '.link-input'
     button_selector = '.button-go'  
     download_button_selector = '.video-links a'
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
 
     # 1. Open URL in Selenium
-    driver = webdriver.Chrome(options=chrome_options)
+    # driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
 
     # 2. Find input, put value
@@ -32,63 +30,33 @@ def snaptik_converter(link_to_video):
     link_element = driver.find_element(By.CSS_SELECTOR, download_button_selector)
     link_url = link_element.get_attribute("href")
     return link_url
-
-    # driver.quit()
-
-snaptik_converter('https://www.tiktok.com/@akodor2012/video/7233034333999680811')
-
-def ssstik_converter(video_url):
+    
+def ssstik_converter(video_url, driver):
     url = 'https://ssstik.io'
-    form_selector = '.pure-form'
-    name_attribute = 'include-vals'
+    input_selector = '#main_page_text'
+    button_selector = '#submit'
     download_link_selector = '.without_watermark'
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
 
     # 1. Open URL in Selenium
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome() #options=chrome_options
     driver.get(url)
-    time.sleep(3)
 
     # 2. Find input, put value
-    form = driver.find_element(By.CSS_SELECTOR, form_selector)
-    token = form.get_attribute(name_attribute)
+    input_element = driver.find_element(By.CSS_SELECTOR, input_selector)
+    input_element.send_keys(video_url)
+    time.sleep(1)
 
-    session = requests.Session()
-    session.get(url)
-    
-    headers = {
-        'authority': 'ssstik.io',
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'hx-current-url': 'https://ssstik.io/en#google_vignette',
-        'hx-request': 'true',
-        'hx-target': 'target',
-        'hx-trigger': '_gcaptcha_pt',
-        'origin': 'https://ssstik.io',
-        'referer': 'https://ssstik.io/en',
-        'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-    }
+    # 3. Find button and click
+    button_element = driver.find_element(By.CSS_SELECTOR, button_selector)
+    button_element.click()
+    time.sleep(5)
 
-    token_key, token_value = token.replace("'", "").split(':')
+    # 4. Find link and retrieve the URL
+    link_element = driver.find_element(By.CSS_SELECTOR, download_link_selector)
+    link_url = link_element.get_attribute("href")
+    driver.quit()
 
-    data = {
-        'id': video_url,
-        'locale': 'en',
-        token_key: token_value
-    }
-
-    response = session.post(url + '/abc', headers=headers, data=data)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    link = soup.select_one(download_link_selector)
-    return link['href']
-    
-ssstik_converter('https://www.tiktok.com/@akodor2012/video/7233034333999680811')
+    return link_url
